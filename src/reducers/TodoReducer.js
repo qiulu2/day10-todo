de2f0fe78bfcf7ca38ac881173abfe7a0d32d302
qiulu2 <qiulu2@oocl.com>
2025-09-14 04:35:31 +0800

    feat: add ADD_TODO and DELETE_TODO actions to todo reducer

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,5 +1,16 @@
 export function todoReducer(state, action) {
     switch (action.type) {
+        case "ADD_TODO":
+            const newTodo = {
+                id: Date.now(),
+                text: action.payload.text,
+                done: false
+            };
+            return [...state, newTodo];
+
+        case "DELETE_TODO":
+            return state.filter(todo => todo.id !== action.payload.id);
+
         case "TOGGLE_TODO":
             /// copy
             const newState = [...state];
