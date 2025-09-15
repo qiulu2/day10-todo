a512da752f9c7964e94b9182fce2c767a1f314fe
qiulu2 <luca.qiu@oocl.com>
2025-09-15 10:51:38 +0800

    refactor(router): implement ErrorPage component for improved error handling

---
+++ src/ErrorPage/ErrorPage.js
@@ -0,0 +1,11 @@
+// TODO errorPage
+import {useRouteError} from "react-router";
+
+export function ErrorPage() {
+    const error = useRouteError();
+    return <div>
+        {error.status === 404
+            ? <div className={"not-found"}><h1>404 Not Found</h1><span>Try</span></div>
+            : <div>{JSON.stringify(error)}</div>}
+    </div>
+}
